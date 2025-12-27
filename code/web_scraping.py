637f07c771dee26dfa596d107e153c4b38bd4354
Anmol Karn <anmolrajpoot98@gmail.com>
2025-12-27 10:56:22 +0530

    Implement web scraping for book data
    This script scrapes book data from 'books.toscrape.com' and saves it to a CSV file.

--- code/web_scraping.py
+++ code/web_scraping.py
@@ -1,1 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Website URL
+url = "https://books.toscrape.com/"
+
+# Send request
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Find all product blocks
+products = soup.find_all("article", class_="product_pod")
+
+# Data list
+data = []
+
+for product in products:
+    # Title
+    title = product.h3.a["title"]
+
+    # Product Link
+    link = url + product.h3.a["href"]
+
+    # Price
+    price = product.find("p", class_="price_color").text
+
+    # Availability
+    availability = product.find("p", class_="instock availability").text.strip()
+
+    # Rating (class name like "star-rating Three")
+    rating = product.p["class"][1]
+
+    # Add to list
+    data.append({
+        "Title": title,
+        "Price": price,
+        "Availability": availability,
+        "Rating": rating,
+        "Link": link
+    })
+
+# Convert to DataFrame
+df = pd.DataFrame(data)
+
+# Save CSV
+df.to_csv("products.csv", index=False)
+
+print("Scraping Completed! File Saved as products.csv")
 
